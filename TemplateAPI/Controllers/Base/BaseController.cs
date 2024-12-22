19810f0344fab65e2c7051f542fbd0e2d7ac6629
asblazquez <asanchezb23@gmail.com>
2024-12-22 21:17:03 +0100

    Reorganización de solución y configuración de proyectos
    Se ha eliminado `TemplateAPI.sln` y añadido `MenuCalendarioAPI.sln` con una estructura de proyectos reorganizada y renombrada.
    
    Se ha añadido el proyecto de base de datos `BBDDMenuCalendario.sqlproj`.
    
    Se han añadido archivos de configuración `appsettings.Development.json` y `appsettings.json` para cadenas de conexión y niveles de registro.
    
    Se ha añadido el proyecto `Presentation.API.csproj` con referencias a `Mapster` y `Swashbuckle.AspNetCore`.
    
    Se ha añadido `Program.cs` para configurar servicios y el pipeline de ASP.NET Core, incluyendo Swagger.
    
    Se ha añadido `TemplateAPI.http` para realizar solicitudes HTTP a la API.
    
    Se ha añadido `BaseController.cs` en `TemplateAPI.Controllers.Base` con configuración básica de rutas y atributos de controlador API.
    
    Se ha añadido `launchSettings.json` con configuraciones de lanzamiento para IIS Express y perfiles de ejecución HTTP y HTTPS.

---
+++ TemplateAPI/Controllers/Base/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateAPI.Controllers.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController
+    {
+    }
+}
